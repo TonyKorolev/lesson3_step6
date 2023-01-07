07e1b38891f3525dcbee60b3a0e1d39e268b8301
KorolevAV <antoshka.992@mail.ru>
2023-01-07 03:16:04 +0300

    update test_items.py, change assert

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(10)
-    IsThereAButton = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
-    assert IsThereAButton, "No button add to basket"
+    buttonArr = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+    assert len(buttonArr) == 1, "No button add to basket"
